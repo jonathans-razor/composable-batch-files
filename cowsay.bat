0c3b71cd7cf6ac8cb03faa976fc2ddf028e01605
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-09-09 15:37:54 -0400

    Sep-9-2023_3:37_PM

---
+++ cowsay.bat
@@ -0,0 +1,56 @@
+:_
+
+@echo off
+if "%~1" == "?" goto help
+goto main
+
+
+
+:_
+:help
+cls
+echo. & echo * Talking cow.
+
+echo. & echo * Usage: %~n0 [space separated parameter(s)]
+
+echo. & echo * Parameter 1: What should the cow say?
+
+echo. & echo * Samples:
+echo   %~n0 Hi mom!
+
+exit/b
+
+rem creation date: 
+
+
+
+:_
+   .--.      .--.      .--.      .--.      .--.                 
+ :::::.\::::::::.\::::::::.\::::::::.\::::::::.\::::::
+        `--'      `--'      `--'      `--'      `--'     
+
+
+
+:_
+:main
+
+rem qq
+echo.
+echo     -----------------
+echo      %*  
+echo     -----------------
+echo                    \       C  C
+echo                     \      ----
+echo                      \    (O O)\________________
+echo                           (   )\                )\/\
+echo                            ---  II              )
+echo                                 II---------W  I
+echo                                 II           II
+echo                                 II           II
+echo.
+echo.
+
+exit/b
+
+
+:_
