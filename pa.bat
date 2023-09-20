de5d57fd4a0d163c4879e0916c96a26ef6135cf7
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-09-20 15:55:14 -0400

    Sep-20-2023_3:55_PM

---
+++ pa.bat
@@ -0,0 +1,45 @@
+:_
+
+@echo off
+if "%~1" == "?" goto help
+goto main
+
+
+
+:_
+:help
+cls
+echo. & echo * Push selected repositories.
+
+echo. & echo   Usage: %~n0 [space separated parameter(s)]
+
+echo. & echo   Batch file style: Single purpose
+
+echo. & echo   Samples:
+echo   %~n0 
+
+exit/b
+
+rem creation date: Sep-20-2023
+
+
+
+:_
+   .--.      .--.      .--.      .--.      .--.                 
+ :::::.\::::::::.\::::::::.\::::::::.\::::::::.\::::::
+        `--'      `--'      `--'      `--'      `--'     
+
+
+
+:_
+:main
+
+call p c
+call p iw
+call p ql
+
+exit/b
+
+
+
+:_
