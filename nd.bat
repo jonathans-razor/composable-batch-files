dbb58c3d73890401efcae61b4319e509a92816dc
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-09-21 17:01:28 -0400

    Sep-21-2023_5:01_PM

--- nd.bat
+++ nd.bat
@@ -3,8 +3,11 @@
 @echo off
 if "%~1" == "?" goto help
 if "%~1" == "" goto help
-if "%~1" == "vers" goto vers
-goto main
+
+echo %1| find /i ".">nul
+if errorlevel 1 goto %1
+
+goto execute-js-file
 
 
 
@@ -16,7 +19,7 @@ echo. & echo * Use Node to compile and run javascript files.
 echo. & echo * Usage: %~n0 [space separated parameter(s)]
 
 echo. & echo * Parameter 1: The name of the javascript file you wish to compile and run.
-echo                        Or to get the node version, type "vers".
+echo                        Or node function you wish to run.
 
 echo. & echo * Samples:
 echo   %~n0 fizz
@@ -45,9 +48,15 @@ exit/b
 
 
 :_
-:main
+:execute-js-file
 
-node %*
+echo. & echo * Execute JS file.
+
+call fe "%~1" & if errorlevel 1 exit/b
+call fnc "%~1" .js & if errorlevel 1 exit/b
+
+echo.
+node "%~1"
 
 exit/b
 
