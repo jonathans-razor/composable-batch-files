1db91bed06e0bd5feedea99cae9b7bfc99ece05f
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-09-29 10:51:21 -0400

    Sep-29-2023_10:51_AM

--- ang.bat
+++ ang.bat
@@ -331,9 +331,30 @@ if %errorlevel% == 1 (
   exit/b 1
 )
 
-
 echo.
 ng generate component components/header
+
+exit/b
+rem lu: Sep-29-2023
+
+
+
+:_
+
+:gbc
+
+echo. & echo * Generate button component.
+
+echo %cd% | find /i "\src\app">nul
+
+if %errorlevel% == 1 (
+  echo.
+  echo * Error: You must be in the src\app folder for this command to work.
+  exit/b 1
+)
+
+echo.
+ng generate component components/button
 
 exit/b
 rem lu: Sep-29-2023
