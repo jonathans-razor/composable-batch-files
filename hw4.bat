b0ae4f65ad9ca3f8d6410e648b5c16d2e26bd609
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-11-04 23:23:54 -0400

    Nov-4-2023_11_23_53_PM

--- hw4.bat
+++ hw4.bat
@@ -25,6 +25,8 @@ echo --------------------  -----------------------------------------------------
 echo           function-1  Function that does [blank].
 echo           function-2  Function that does [blank].
 echo           function-3  Function in ANOTHER batch file that does [blank].
+echo           function-4  Call function 3 in ANOTHER batch file.
+echo           function-5  Echo parameters.
 echo        all-functions  Function that runs all functions.
 echo                  4ds  Same function called 2 different syntaxes.
 
@@ -59,6 +61,30 @@ exit/b
 
 
 :_
+:function-4
+cls
+echo. & echo * Call function 3 in ANOTHER batch file.
+call hw5 function-1
+
+exit/b
+
+
+
+:_
+:function-5
+cls
+echo. & echo * Echo parameters.
+
+echo. & echo * Parameter 1: %1
+echo. & echo * Parameter 2: %2
+echo. & echo * Parameter 3: %3
+echo. & echo * All parameters: %*
+
+exit/b
+
+
+
+:_
 :all-functions
 echo. & echo * All functions.
 call :function-1
@@ -76,16 +102,6 @@ call hw4 function-3
 call %0 function-3
 goto function-3
 rem Question: What is the difference between "call" and "goto"?
-exit/b
-
-
-
-:_
-:function-4
-cls
-echo. & echo * Function 3 in another batch file.
-call hw5 function-1
-
 exit/b
 
 
