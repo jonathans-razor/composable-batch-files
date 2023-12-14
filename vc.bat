6a739774c37e06f096caa69ee2601b1e3c3c1a69
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-14 15:32:59 -0500

    Dec-14-2023_3_32_59_PM

--- vc.bat
+++ vc.bat
@@ -55,6 +55,28 @@ exit/b
 
 
 :_
+:vers
+
+echo. & echo * Version.
+
+exit/b
+
+
+On XPS on Dec-14-2023:
+
+Version: 1.85.1 (user setup)
+Commit: 0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2
+Date: 2023-12-13T09:49:37.021Z
+Electron: 25.9.7
+ElectronBuildId: 25551756
+Chromium: 114.0.5735.289
+Node.js: 18.15.0
+V8: 11.4.183.29-electron.0
+OS: Windows_NT x64 10.0.19045
+
+
+
+:_
 :main
 e.bat %* /e:%0
 exit/b
