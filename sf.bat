353d17e5389d9cbb64ee190cc09b9d9ddbbbf205
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-05 18:16:25 -0500

    Jan-5-2024_6_16_25_PM

--- sf.bat
+++ sf.bat
@@ -57,9 +57,10 @@ call i /c>nul
 
 rem echo. & echo * Evaluate the parameter list.
 
-call n %1
-         
-if errorlevel 1 exit/b
+call n %1 || exit/b
+rem qq
+
+echo. & echo * Error Level: %errorlevel% - qjq - cbf-: %cbf-% - Jan-5-2024_6_15_PM
 
 if "%cbf-app%" == "" (
   call :use_default_browser
