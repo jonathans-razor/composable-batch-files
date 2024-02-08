e97c0a4cb305525080f0ad1cb2c346a19aef8e6e
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-08 12:56:00 -0500

    Feb-8-2024-12-56-00-PM

--- sdv.bat
+++ sdv.bat
@@ -43,16 +43,17 @@ Feb-7-2024
 :main
 
 rem Feb-7-2024: new double dictionary code.
-call n %1 2>nul
-
-rem The alias is found in the primary dictionary, so exit.
-if not errorlevel 1 exit/b
 
 if not exist "%cbf-default-sd%" exit/b
 
+call "%cbf-default-sd%" %1 2>nul
+
+rem The alias is found in the secondary dictionary, so exit.
+if not errorlevel 1 exit/b
+
 call el /c>nul
 
-call "%cbf-default-sd%" %1 2>nul
+call n %1 2>nul
 rem qq
 
 if errorlevel 1 err %0.bat: A dictionary entry for "%1" was not found. Feb-7-2024-7-00-PM
