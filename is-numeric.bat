8bdd63301100706baae2aec8a3cbb7a8f6b51a93
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-07 14:19:07 -0500

    Mar-7-2024-2-19-07-PM

--- is-numeric.bat
+++ is-numeric.bat
@@ -3,7 +3,7 @@
 
 title CBF: %0
 
-if "%~1" == "" err Parameter 1 is required.
+if "%~1" == "" not Parameter 1 is required.
 if "%~1" == "?" goto help
 
 goto main
@@ -49,7 +49,10 @@ rem echo. & echo * When the two are equal, the variable or parameter is numeric.
 
 set /a numeric_checker=%~1
 rem echo. & echo nc: %numeric_checker%
-if %numeric_checker% neq %~1 exit/b 1
+if %numeric_checker% neq %~1 (
+  call err Numerical data was expected in argument 1. Mar-7-2024-2-18-PM
+  exit/b 1
+)
 
 exit/b 0
 
