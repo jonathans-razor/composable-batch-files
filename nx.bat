bb1bf804eb60857b37bbce204773d60696ccdadf
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-04-05 16:34:56 -0400

    Apr-5-2024-4-34-56-PM

--- nx.bat
+++ nx.bat
@@ -224,8 +224,9 @@ echo. & echo * Install miscellaneous.
 
 @echo on
 rem qq
-npx shadcn-vue@latest add toast
+npx shadcn-vue@latest add select
 exit/b
+npx shadcn-vue@latest add toast
 npx shadcn-vue@latest add table
 npx shadcn-vue@latest add tooltip
 npx shadcn-vue@latest add label
