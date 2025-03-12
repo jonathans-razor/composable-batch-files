c8b6a8d2260d7e7c4a0284c0d794ff855d4dba47
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2025-03-12 15:17:38 -0400

    Mar-12-2025-3-17-37-PM

--- ap.bat
+++ ap.bat
@@ -5,6 +5,8 @@ echo.
 echo * Price log.
 
 echo.
+echo   215 Mar-12-2025
+echo   221 Mar-12-2025
 echo   225 Mar-10-2025
 echo   239 Mar-7-2025
 echo   248 Feb-24-2025
--- ht.bat
+++ ht.bat
@@ -4,6 +4,7 @@ echo.
 echo * Price log.
 
 echo.
+echo   18.01 Mar-10-2025
 echo   18.48 Mar-10-2025
 echo   18.89 Mar-7-2025
 echo   21.50 Feb-24-2025
