967ebd9647eb7e128a31f4569dce1bbb25beb1d4
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2025-05-19 16:15:31 -0400

    May-19-2025-4-15-31-PM

--- p.bat
+++ p.bat
@@ -78,6 +78,11 @@ call t f || exit/b
 call g dirt || exit/b
 
 :
+call sep aa-diamond-bridge
+call t vu || exit/b
+call g dirt || exit/b
+
+:
 rem echo. & echo * Exiting here ***************************************.
 call rf>nul
 echo.
