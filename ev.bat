8476d525565fd91bc325961b30c4576dfd7b82c8
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2025-05-22 15:59:40 -0400

    May-22-2025-3-59-40-PM

--- ev.bat
+++ ev.bat
@@ -2,17 +2,19 @@
 @echo off
 if "%~1" == "?" goto help
 goto %1
+goto main
+goto preprocess
 
 
 
 :_
 :help
 cls
-echo. & echo * Hello worlds.
+echo. & echo * Evolver. Goes beyond hello world.
 echo. & echo   Usage: 
 echo   %~n0 [space separated parameter(s)]
 echo. & echo   Parameter 1:
-echo   If equal to 'all', all hello worlds are run.
+echo   x
 echo. & echo   Creation Date:
 echo    Apr-22-2024
 echo. & echo   Samples:
@@ -30,38 +32,13 @@ exit/b
 
 
 :_
-:all
-sl hw
-exit/b
+:preprocess
 
 
 
 :_
-:qs
-:qs2
-echo. & echo * Hello world 2 from Quasar.
-call t qhw2
-call qs dev
-exit/b
-cd:
-May-22-2025: Created with the Quasar CLI option, not the 'UI' option.
+:main
 
-
-
-:_
-:vu
-echo. & echo * Hello world from Vue.js.
-call t hw2
-call nm srv
-call lh 8080
-exit/b
-
-
-
-:_
-:vus
-echo. & echo * Vue.js - simple version.
-call fx vuejs-hw fn br
 exit/b
 
 
